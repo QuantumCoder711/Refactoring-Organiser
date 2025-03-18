7e69ae1319b51ac1bf9a7bb84222579682a8fde6
Lavlesh <lavlesh@klout.club>
2025-03-18 11:06:30 +0530

    Making Total Events Cards

--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Dashboard:React.FC = () => {
   return (
     <div>
-      <h1>Dashboard</h1>
+      {/* Events Information */}
+      <div className='grid grid-cols-4'>
+        <div className='bg-brand-background rounded-lg p-4'>Total Events</div>
+      </div>
     </div>
   )
 }
--- src/pages/admin/layout.tsx
+++ src/pages/admin/layout.tsx
@@ -3,15 +3,15 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../../components/Navbar';
 const Layout: React.FC = () => {
     return (
-        <main className='h-screen overflow-hidden p-2 w-full flex flex-col flex-1'>
+        <main className='h-screen overflow-hidden w-full flex flex-col flex-1'>
             <Navbar isAuthenticated={true} />
 
-            <div className="flex flex-1">
-                <aside className='bg-red-500 w-52 lg:w-56'></aside>
-                <div className='border-2 border-yellow-500 flex-1 p-10'>
+            <section className="flex flex-1">
+                <aside className='w-52 lg:w-56'></aside>
+                <div className='flex-1 p-10'>
                     <Outlet />
                 </div>
-            </div>
+            </section>
         </main>
     )
 }
