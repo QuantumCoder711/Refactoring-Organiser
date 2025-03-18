5a253bbde8f1c6f5ff08aa7d3ea7a593f48ccf8f
Lavlesh <lavlesh@klout.club>
2025-03-18 11:25:23 +0530

    Making Cards

--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
-const Dashboard:React.FC = () => {
+const Dashboard: React.FC = () => {
   return (
     <div>
       {/* Events Information */}
-      <div className='grid grid-cols-4'>
-        <div className='bg-brand-background rounded-lg p-4'>Total Events</div>
+      <div className='grid grid-cols-4 gap-5'>
+
+        {/* Card 1 */}
+        <div className='cursor-pointer duration-300 hover:bg-brand-background rounded-lg h-9 px-4 shadow-[0_0_4px_0_rgba(0,0,0,0.25)] flex justify-between items-center'>
+          <span>Total Events</span>
+          <span>10</span>
+        </div>
+        {/* Card 2 */}
+        <div className='cursor-pointer duration-300 hover:bg-brand-background rounded-lg h-9 px-4 shadow-[0_0_4px_0_rgba(0,0,0,0.25)] flex justify-between items-center'>
+          <span>Total Attendees</span>
+          <span>10</span>
+        </div>
+        {/* Card 3 */}
+        <div className='cursor-pointer duration-300 hover:bg-brand-background rounded-lg h-9 px-4 shadow-[0_0_4px_0_rgba(0,0,0,0.25)] flex justify-between items-center'>
+          <span>Total Sponsers</span>
+          <span>10</span>
+        </div>
+        {/* Card 4 */}
+        <div className='cursor-pointer duration-300 hover:bg-brand-background rounded-lg h-9 px-4 shadow-[0_0_4px_0_rgba(0,0,0,0.25)] flex justify-between items-center'>
+          <span>Upcoming Events</span>
+          <span>10</span>
+        </div>
       </div>
     </div>
   )
