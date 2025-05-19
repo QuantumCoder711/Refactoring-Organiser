eb9121fe99455c61de722992980dc660c1891373
Lavlesh <lavlesh@klout.club>
2025-05-19 11:48:03 +0530

    Pie Chart Made

--- src/components/PieChart.tsx
+++ src/components/PieChart.tsx
@@ -4,7 +4,6 @@ import { Label, Pie, PieChart } from "recharts"
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
@@ -17,71 +16,38 @@ import {
 } from "@/components/ui/chart";
 
 const colors: string[] = [
-  "hsl(210, 20%, 50%)", // slate-500
-  "hsl(0, 0%, 50%)", // gray-500
-  "hsl(240, 5%, 50%)", // zinc-500
-  "hsl(0, 0%, 50%)", // neutral-500
-  "hsl(30, 6%, 50%)", // stone-500
-  "hsl(0, 100%, 50%)", // red-500
-  "hsl(30, 100%, 50%)", // orange-500
-  "hsl(45, 100%, 50%)", // amber-500
-  "hsl(60, 100%, 50%)", // yellow-500
-  "hsl(90, 100%, 50%)", // lime-500
-  "hsl(120, 100%, 50%)", // green-500
-  "hsl(150, 100%, 50%)", // emerald-500
-  "hsl(180, 100%, 50%)", // teal-500
   "hsl(210, 100%, 50%)", // cyan-500
-  "hsl(240, 100%, 50%)", // sky-500
-  "hsl(270, 100%, 50%)", // blue-500
-  "hsl(300, 100%, 50%)", // indigo-500
-  "hsl(330, 100%, 50%)", // violet-500
-  "hsl(360, 100%, 50%)", // purple-500
-  "hsl(30, 100%, 50%)", // fuchsia-500
-  "hsl(60, 100%, 50%)", // pink-500
-  "hsl(90, 100%, 50%)" // rose-500
+  "hsl(30, 100%, 50%)", // orange-500
 ];
-
-const chartData = [
-  { browser: "chrome", visitors: 275, fill: colors[13] },
-  { browser: "safari", visitors: 200, fill: colors[6] },
-]
 
 const chartConfig = {
   visitors: {
-    label: "Visitors",
+    label: "Attendees",
   },
-  chrome: {
-    label: "Chrome",
-    color: "hsl(var(--chart-1))",
+  checkedIn: {
+    label: "Checked In",
+    color: colors[0],
   },
-  safari: {
-    label: "Safari",
-    color: "hsl(var(--chart-2))",
-  },
-  firefox: {
-    label: "Firefox",
-    color: "hsl(var(--chart-3))",
-  },
-  edge: {
-    label: "Edge",
-    color: "hsl(var(--chart-4))",
-  },
-  other: {
-    label: "Other",
-    color: "hsl(var(--chart-5))",
+  nonCheckedIn: {
+    label: "Non Checked In",
+    color: colors[1],
   },
 } satisfies ChartConfig
 
-export function PieChartComponent() {
+export function PieChartComponent(props: { checkedInUsers: number; nonCheckedInUsers: number }) {
+  const chartData = [
+    { status: "Checked In", visitors: props.checkedInUsers, fill: colors[0] },
+    { status: "Non Checked In", visitors: props.nonCheckedInUsers, fill: colors[1] },
+  ]
+  
   const totalVisitors = React.useMemo(() => {
     return chartData.reduce((acc, curr) => acc + curr.visitors, 0)
-  }, [])
+  }, [chartData])
 
   return (
     <Card className="flex flex-col bg-transparent border-none shadow-none">
       <CardHeader className="items-center pb-0">
         <CardTitle className="text-center text-xl">Total Attendees</CardTitle>
-        {/* <CardDescription>January - June 2024</CardDescription> */}
       </CardHeader>
       <CardContent className="flex-1 pb-0">
         <ChartContainer
@@ -91,12 +57,12 @@ export function PieChartComponent() {
           <PieChart>
             <ChartTooltip
               cursor={false}
-              content={<ChartTooltipContent hideLabel />}
+              content={<ChartTooltipContent />}
             />
             <Pie
               data={chartData}
               dataKey="visitors"
-              nameKey="browser"
+              nameKey="status"
               innerRadius={60}
               strokeWidth={5}
             >
@@ -122,7 +88,7 @@ export function PieChartComponent() {
                           y={(viewBox.cy || 0) + 24}
                           className="fill-muted-foreground"
                         >
-                          Visitors
+                          Total Attendees
                         </tspan>
                       </text>
                     )
@@ -134,7 +100,6 @@ export function PieChartComponent() {
         </ChartContainer>
       </CardContent>
       <CardFooter className="flex-col gap-2 text-sm">
-        
       </CardFooter>
     </Card>
   )
--- src/pages/admin/all-reports/charts/index.tsx
+++ src/pages/admin/all-reports/charts/index.tsx
@@ -4,16 +4,37 @@ import { HorizontalBarChartComponent } from '@/components/HorizontalBarChart';
 import { PieChartComponent } from '@/components/PieChart';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
-import React from 'react'
+import useAttendeeStore from '@/store/attendeeStore';
+import useAuthStore from '@/store/authStore';
+import useEventStore from '@/store/eventStore';
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom';
 
-const Charts:React.FC = () => {
+const Charts: React.FC = () => {
+  const { slug } = useParams<{ slug: string }>();
+  const { token } = useAuthStore(state => state);
+  const event = useEventStore(state => state.getEventBySlug(slug));
+  const { singleEventAttendees, getSingleEventAttendees } = useAttendeeStore(state => state);
+
+  useEffect(() => {
+    if (token && event?.uuid) {
+      getSingleEventAttendees(token, event.uuid);
+    }
+  }, [token, slug, event]);
+
+
+  const allCheckedInUsers = singleEventAttendees.filter(attendee => attendee.check_in === 1);
+  const allNonCheckedInUsers = singleEventAttendees.filter(attendee => attendee.check_in === 0);
+
+  console.log(allCheckedInUsers, allNonCheckedInUsers);
+
   return (
     <div className='flex justify-between h-full'>
       <GoBack />
       <div className='max-w-3xl rounded-[10px] min-h-full w-full bg-brand-background p-2'>
-        <PieChartComponent />
+        <PieChartComponent checkedInUsers={allCheckedInUsers.length} nonCheckedInUsers={allNonCheckedInUsers.length} />
         <BarChartComponent />
-        <Separator className='bg-black'/>
+        <Separator className='bg-black' />
         <HorizontalBarChartComponent />
       </div>
       <Button>Export Charts</Button>
