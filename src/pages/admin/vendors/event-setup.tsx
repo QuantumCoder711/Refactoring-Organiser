e08ad9f604b92e7f9e52f925338cb21a85421517
Lavlesh <lavlesh@klout.club>
2025-07-09 18:12:25 +0530

    Vendors Search Added

--- src/pages/admin/vendors/event-setup.tsx
+++ src/pages/admin/vendors/event-setup.tsx
@@ -3,14 +3,47 @@ import GoBack from '@/components/GoBack';
 import useVendorStore from '@/store/vendorStore';
 import Wave from '@/components/Wave';
 import VendorCompanyCard from '@/components/VendorCompanyCard';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
+import { VendorCompanyType } from '@/types';
+
+const cityList: string[] = [
+  "Delhi",
+  "Mumbai",
+  "Bangalore",
+  "Hyderabad",
+  "Chennai",
+  "Kolkata",
+  "Pune",
+  "Ahmedabad",
+  "Surat",
+  "Visakhapatnam",
+]
 
 const EventSetupPage: React.FC = () => {
 
   const { eventSetupVendors, loading, getEventSetupVendors } = useVendorStore();
+  const [selectedCity, setSelectedCity] = React.useState<string>("");
+  const [filteredVendors, setFilteredVendors] = React.useState<VendorCompanyType[]>([]);
 
   useEffect(() => {
     getEventSetupVendors();
   }, []);
+
+  useEffect(() => {
+    if(selectedCity === "all" || selectedCity === "") {
+      setFilteredVendors(eventSetupVendors);
+    } else {
+      const filtered = eventSetupVendors.filter((vendor) => vendor.city.includes(selectedCity.toLowerCase()));
+      setFilteredVendors(filtered);
+    }
+  }, [selectedCity, eventSetupVendors]);
 
   if (loading) {
     return <Wave />
@@ -23,9 +56,30 @@ const EventSetupPage: React.FC = () => {
         <h1 className="text-2xl font-bold">Event Setup</h1>
       </div>
 
+      <div>
+        <div className="mt-10 !mx-auto max-w-fit">
+          <Label className='mb-3'>Select City</Label>
+          <Select onValueChange={(value) => setSelectedCity(value)} defaultValue="all">
+            <SelectTrigger className="input min-w-96 text-base cursor-pointer">
+              <SelectValue placeholder="Select City"/>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem key="all" value="all" className="cursor-pointer">
+                All
+              </SelectItem>
+              {cityList.map((city) => (
+                <SelectItem key={city} value={city} className="cursor-pointer">
+                  {city}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+
       <div className="mt-10 flex gap-5">
         {/* Company Card */}
-        {eventSetupVendors.map((company, index) => (
+        {filteredVendors.map((company, index) => (
           <VendorCompanyCard key={index} company={company} />
         ))}
       </div>
--- src/pages/admin/vendors/gifting.tsx
+++ src/pages/admin/vendors/gifting.tsx
@@ -3,14 +3,47 @@ import GoBack from '@/components/GoBack';
 import useVendorStore from '@/store/vendorStore';
 import Wave from '@/components/Wave';
 import VendorCompanyCard from '@/components/VendorCompanyCard';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
+import { VendorCompanyType } from '@/types';
+
+const cityList: string[] = [
+  "Delhi",
+  "Mumbai",
+  "Bangalore",
+  "Hyderabad",
+  "Chennai",
+  "Kolkata",
+  "Pune",
+  "Ahmedabad",
+  "Surat",
+  "Visakhapatnam",
+]
 
 const GiftingPage: React.FC = () => {
 
   const { giftingVendors, loading, getGiftingVendors } = useVendorStore();
+  const [selectedCity, setSelectedCity] = React.useState<string>("");
+  const [filteredVendors, setFilteredVendors] = React.useState<VendorCompanyType[]>([]);
 
   useEffect(() => {
     getGiftingVendors();
   }, []);
+
+  useEffect(() => {
+    if(selectedCity === "all" || selectedCity === "") {
+      setFilteredVendors(giftingVendors);
+    } else {
+      const filtered = giftingVendors.filter((vendor) => vendor.city.includes(selectedCity.toLowerCase()));
+      setFilteredVendors(filtered);
+    }
+  }, [selectedCity, giftingVendors]);
 
   if (loading) {
     return <Wave />
@@ -23,9 +56,30 @@ const GiftingPage: React.FC = () => {
         <h1 className="text-2xl font-bold">Gifting</h1>
       </div>
 
+      <div>
+        <div className="mt-10 !mx-auto max-w-fit">
+          <Label className='mb-3'>Select City</Label>
+          <Select onValueChange={(value) => setSelectedCity(value)} defaultValue="all">
+            <SelectTrigger className="input min-w-96 text-base cursor-pointer">
+              <SelectValue placeholder="Select City"/>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem key="all" value="all" className="cursor-pointer">
+                All
+              </SelectItem>
+              {cityList.map((city) => (
+                <SelectItem key={city} value={city} className="cursor-pointer">
+                  {city}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+
       <div className="mt-10 flex gap-5">
         {/* Company Card */}
-        {giftingVendors.map((company, index) => (
+        {filteredVendors.map((company, index) => (
           <VendorCompanyCard key={index} company={company} />
         ))}
       </div>
