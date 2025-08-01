36029bb3899c87a952ce29ff59f4a576431a068c
Lavlesh <lavlesh@klout.club>
2025-08-01 14:04:29 +0530

    Images Optimised

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import viteImagemin from "vite-plugin-imagemin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteImagemin({
+      pngquant: {
+        quality: [0.6, 0.8], // Set the quality range for PNG images
+      },
+      webp: {
+        quality: 75, // Convert PNGs and other supported formats to WebP with this quality
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
