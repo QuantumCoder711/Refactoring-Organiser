0c5db8e08e98fc9fad10375e97e9ae352a79b56d
Lavlesh <lavlesh@klout.club>
2025-08-11 11:15:59 +0530

    gtag-init.ts script added

---
+++ gtag-init.ts
@@ -0,0 +1,8 @@
+// @ts-ignore
+window.dataLayer = window.dataLayer || [];
+// @ts-ignore
+function gtag() { dataLayer.push(arguments); }
+// @ts-ignore
+gtag('js', new Date());
+// @ts-ignore
+gtag('config', 'AW-16883967963');
