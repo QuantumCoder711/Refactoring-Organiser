3846bd7aa231bf8adfef3f5c64b82465ca269980
Lavlesh <lavlesh@klout.club>
2025-09-05 12:37:58 +0530

    Banner Updated

--- src/types/index.ts
+++ src/types/index.ts
@@ -234,6 +234,7 @@ export interface MessageTemplateType {
     hour_interval: number;
     status: number;
     check_in: number;
+    template_banner?: File | string | null;
 }
 
 export interface SendReminderType extends Omit<MessageTemplateType, 'check_in'> { }
